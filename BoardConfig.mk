1e26b32a8b2b5bfcbd17edf68cd2c6a072379744
Shaka Huang <shakalaca@gmail.com>
2017-04-08 23:30:26 +0800

    SELinux: Enforcing
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,7 +56,7 @@ TARGET_USE_SDCLANG := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci 
-BOARD_KERNEL_CMDLINE += earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += earlycon=msm_hsl_uart,0x78af000
 BOARD_KERNEL_PAGESIZE    := 2048
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00008000
@@ -200,7 +200,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
