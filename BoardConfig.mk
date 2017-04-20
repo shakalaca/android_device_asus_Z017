25907ef7be9467c28c61991e30ad84794a20fe90
Shaka Huang <shakalaca@gmail.com>
2017-04-20 20:36:49 +0800

    Use texfat kernel module instead
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,6 +193,7 @@ TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/etc/fstab.qcom
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+TARGET_KERNEL_HAVE_EXFAT := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
