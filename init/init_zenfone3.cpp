9e0ff140760169d92164116bc5f3c6dc250cae02
Shaka Huang <shakalaca@gmail.com>
2017-05-05 20:02:22 +0800

    Sync 14.2020.1703.28 properties
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- init/init_zenfone3.cpp
+++ init/init_zenfone3.cpp
@@ -38,19 +38,21 @@ void vendor_load_properties()
     property_set("ro.product.name", "WW_Phone");
     if (project == "6") {
         property_set("ro.build.product", "ZE520KL");
+        property_set("ro.build.description", "WW_Phone-user 7.0 NRD90M 14.2020.1703.28-20170410 release-keys");
+        property_set("ro.build.fingerprint", "asus/WW_Phone/ASUS_Z017D_1:7.0/NRD90M/14.2020.1703.28-20170410:user/release-keys");
         property_set("ro.product.device", "ASUS_Z017D_1");
         property_set("ro.product.model", "ASUS_Z017D");
-        property_set("ro.build.description", "WW_Phone-user 7.0 NRD90M 14.2015.1701.8-20170222 release-keys");
-        property_set("ro.build.fingerprint", "asus/WW_Phone/ASUS_Z017D_1:7.0/NRD90M/14.2015.1701.8-20170222:user/release-keys");
         property_set("ro.product.carrier", "US-ASUS_Z017D-WW_Phone");
         property_set("ro.hardware.id", "ZE520KL_MP");
+        property_set("ro.build.csc.version", "WW_ZE520KL-14.2020.1703.28-20170410");
     } else if (project == "7") {
         property_set("ro.build.product", "ZE552KL");
+        property_set("ro.build.description", "WW_Phone-user 7.0 NRD90M 14.2020.1703.28-20170410 release-keys");
+        property_set("ro.build.fingerprint", "asus/WW_Phone/ASUS_Z012D:7.0/NRD90M/14.2020.1703.28-20170410:user/release-keys");
         property_set("ro.product.device", "ASUS_Z012D");
         property_set("ro.product.model", "ASUS_Z012D");
-        property_set("ro.build.description", "WW_Phone-user 7.0 NRD90M 14.2015.1701.8-20170222 release-keys");
-        property_set("ro.build.fingerprint", "asus/WW_Phone/ASUS_Z012D:7.0/NRD90M/14.2015.1701.8-20170222:user/release-keys");
         property_set("ro.product.carrier", "US-ASUS_Z012D-WW_Phone");
         property_set("ro.hardware.id", "ZE552KL_MP");
+        property_set("ro.build.csc.version", "WW_ZE552KL-14.2020.1703.28-20170410");
     }
 }
