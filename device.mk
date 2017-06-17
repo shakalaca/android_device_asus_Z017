b845c3c3b7e406592af3b87f1ecc8f5654b7975a
Shaka Huang <shakalaca@gmail.com>
2017-06-17 10:09:35 +0800

    build libbthost_if from source
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- device.mk
+++ device.mk
@@ -81,6 +81,7 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
+    libbthost_if \
     libaudio-resampler \
     libqcomvisualizer \
     libqcomvoiceprocessing \
